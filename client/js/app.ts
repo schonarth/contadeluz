b543c0e13486ce79216cfc072e1b0a8db33e800d
schonarth <somatando@gmail.com>
2015-09-04 19:22:59 +0000

    Passando os 3 primeiros testes: alimentando lista básica de aparelhos.

--- client/js/app.ts
+++ client/js/app.ts
@@ -1,5 +1,11 @@
 'use strict';
 (function(){
+	interface Aparelho {
+		nome: string;
+		potenciaPadrao: number;
+		horasUsoPadrao: number;
+	}
+	
 	var app = angular.module('ContaDeLuz', []); //['ngResource', 'ui.bootstrap']);
 	
 	app.config(function($logProvider, $locationProvider){
@@ -17,10 +23,26 @@
 		// */
 	});
 	
-	app.run(function($rootScope, $log){
+	app.run(function($rootScope, $log, AparelhosFactory){
 		
 		$log.info("Angulando...", $rootScope);
 		
+		$rootScope.aparelhos = AparelhosFactory.getAparelhos();
+	});
+	
+	app.factory('AparelhosFactory', function(){
+		return {
+			getAparelhos: function(){
+				var aparelhos: Aparelho[] = [];
+				aparelhos.push({
+					nome: "Ar condicionado",
+					potenciaPadrao: 1400,
+					horasUsoPadrao: 0
+				});
+
+				return aparelhos;
+			}
+		};
 	});
 	
 })();
--- test/unit/ui.test.js
+++ test/unit/ui.test.js
@@ -1,14 +1,16 @@
 'use strict';
 // angular.module('importar módulo adicional aqui', []).constant("CONSTANTE_AQUI", null);
 
-//beforeEach(module('importar módulo a ser testadp aqui'))
-//*
+beforeEach(module('ContaDeLuz'))
+
 describe("Conta de Luz", function() {
 	var $rootScope, scope;
 	
-	beforeEach(inject(function(_$rootScope_) {
+	beforeEach(inject(function(_$rootScope_, AparelhosFactory) {
 		$rootScope = _$rootScope_;
-		scope = $rootScope.$new();
+		$rootScope.aparelhos = AparelhosFactory.getAparelhos();
+		
+		//scope = $rootScope.$new();
 		
 		// declara o controller e injeta o novo scope 
 		//$controller('NomeDoController', {$scope: scope});
@@ -16,6 +18,22 @@ describe("Conta de Luz", function() {
 	
 	it("roda a app", function() {
 		console.info("Rodando app angular...");
+		expect($rootScope).not.toBe(undefined);
 		expect($rootScope).not.toBe(null);
+	});
+	
+	it("tem a lista de aparelhos", function() {
+		var aparelhos = $rootScope.aparelhos;
+		console.debug("Aparelhos", aparelhos);
+		expect(aparelhos).not.toBe(undefined);
+		expect(aparelhos.length).toBeGreaterThan(0);
+	});
+		
+	it("um aparelho tem suas propriedades básicas", function() {
+		var aparelho = $rootScope.aparelhos[0];
+		expect(aparelho).not.toBe(undefined);
+		expect(aparelho.nome).not.toBe(undefined);
+		expect(aparelho.potenciaPadrao).not.toBe(undefined);
+		expect(aparelho.horasUsoPadrao).not.toBe(undefined);
 	});
 });
